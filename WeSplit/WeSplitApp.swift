fd4bed31a9ac8052b3159d91a1a6345c52b73345
Dannyung50 <olaleyedaniel16@gmail.com>
2023-08-27 05:27:01 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Olaleye Daniel Ifeoluwa on 27/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
